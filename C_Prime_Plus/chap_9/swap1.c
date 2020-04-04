0aa1a4c6d302cacafa78bb8c71f91475b2f6f3ca
Sunshine <xxhan2018@163.com>
2020-04-04 15:05:05 +0800

    add chap_9, chap_10

---
+++ C_Prime_Plus/chap_9/swap1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void interchange(int u, int v);
+
+int main(void)
+{
+    int x = 5, y = 10;
+
+    printf("Original x = %d and y = %d.\n", x, y);
+
+    interchange(x, y);
+    printf("Now x = %d and y = %d.\n", x, y);
+}
+
+void interchange(int u, int v)
+{
+    int temp;
+    temp = u;
+    u = v;
+    v = temp;
+}
