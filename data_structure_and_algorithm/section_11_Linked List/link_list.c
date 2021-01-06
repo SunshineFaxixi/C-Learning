d6e4b5343ab1aa62fc44f28b1950a406e16813f5
Sunshine <xxhan2018@163.com>
2021-01-06 08:31:35 +0800

    update files

--- data_structure_and_algorithm/section_11_Linked List/link_list.c
+++ data_structure_and_algorithm/section_11_Linked List/link_list.c
@@ -53,16 +53,97 @@ void Reverse_RDisplay(struct Node *p)
     }
 }
 
+int count(struct Node *p)
+{
+    int l = 0;
+    while(p)
+    {
+        l++;
+        p = p->next;
+    }
+
+    return l;
+}
+
+int Rcount(struct Node *p)
+{
+    if(p != NULL)
+    {
+        return Rcount(p->next) + 1;
+    }
+    else
+        return 0;
+}
+
+int sum(struct Node *p)
+{
+    int s = 0;
+
+    while(p)
+    {
+        s += p->data;
+        p = p->next;
+    }
+
+    return s;
+}
+
+int Rsum(struct Node *p)
+{
+    if(p == NULL)
+        return 0;
+    else
+        return Rsum(p->next) + p->data;
+}
+
+int max(struct Node *p)
+{
+    int m = INT_MIN;
+    while(p)
+    {
+        if(p->data > m)
+            m = p->data;
+        p = p->next;
+    }
+
+    return m;
+}
+
+int Rmax(struct Node *p)
+{
+    int x = 0;
+
+    if(p == NULL)
+        return INT_MIN;
+    x = Rmax(p->next);
+    return x > p->data ? x : p->data;
+}
+
+struct Node *LSearch(struct Node *p)
+{
+
+}
+
 int main()
 {
-    int A[] = {3, 5, 7, 10, 15};
-    create(A, 5);
+    int A[] = {43, 5, 7, 40, 25, 20, 53};
+    create(A, 7);
     printf("\nwhile loop:\n");
     display(first);
     printf("\nrecursion:\n");
     RDisplay(first);
     printf("\nreverse print:\n");
     Reverse_RDisplay(first);
+    
+    printf("\n");
+    printf("Length is %d\n", count(first));
+    printf("RLength is %d\n", Rcount(first));
+
+    printf("Sum is %d\n", sum(first));
+    printf("RSum is %d\n", Rsum(first));
+
+    printf("Max is %d\n", max(first));
+    printf("RMax is %d\n", Rmax(first));
 
     return 0;
 }
