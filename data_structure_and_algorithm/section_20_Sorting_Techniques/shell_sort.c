63cdc9ff039b9faa7e8fe55fa680d4e0045ffb18
Sunshine <xxhan2018@163.com>
2021-02-10 08:23:38 +0800

    add shell sort

---
+++ data_structure_and_algorithm/section_20_Sorting_Techniques/shell_sort.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+void ShellSort(int A[], int n)
+{
+    int gap, i, j, temp;
+
+    for(gap = n/2; gap >= 1; gap /= 2)
+    {
+        for(i = gap; i < n; i++)
+        {
+            temp = A[i];
+            j = i - gap;
+            while(j >= 0 && A[j] > temp)
+            {
+                A[j + gap] = A[j];
+                j = j - gap;
+            }
+            A[j + gap] = temp;
+        }
+    }
+}
+
+int main()
+{
+    int A[] = {3, 7, 9, 10, 6, 5, 12, 4, 11, 2};
+    int n = sizeof(A) / sizeof(int);
+    int i;
+
+    ShellSort(A, n);
+
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", A[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
